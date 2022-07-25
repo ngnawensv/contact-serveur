8f686f7d69e3a1799213c76790ee8b07ec704e8f
Ngnawen Samuel <ngnawens@gmail.com>
2022-07-25 13:31:26 +0400

    First commit

---
+++ src/main/java/cm/belrose/contactserveur/ContactServeurApplication.java
@@ -0,0 +1,13 @@
+package cm.belrose.contactserveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactServeurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cm/belrose/contactserveur/ContactServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.belrose.contactserveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
